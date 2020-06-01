52d35310647351e0b7b67fc27b4c5a0361b78223
Tan Wei Jin <weijin_tan@mymail.sutd.edu.sg>
2020-06-01 12:33:25 +0800

    Update animations with mouse

--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -17,26 +17,15 @@ public class player_movement : MonoBehaviour
     {
         // Get movement of wasd keys
         Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f);
-
-        if (movement != Vector3.zero) {
-            anim.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal")); // using raw to change animation immediately
-            anim.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
-        }
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        // Get vector that points from player to mouse position
+        Vector2 lookDir = mousePosition - rb.position;
+        // Get angle of vector
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        anim.SetFloat("Mouse", angle);
         anim.SetFloat("Magnitude", movement.magnitude);
 
         //mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
         transform.position = transform.position + movement * Time.deltaTime;
-    }
-
-    void FixedUpdate()
-    {
-        //update player position
-        //rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
-        // Get vector that points from player to mouse position
-        //Vector2 lookDir = mousePosition - rb.position;
-        // Get angle of vector
-        //float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-        //update player rotation
-        //rb.rotation = angle;
     }
 }
