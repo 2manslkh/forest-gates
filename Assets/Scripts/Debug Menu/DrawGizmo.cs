e6c0aa0948b9775737fc20a80d6076a76ca62849
Lim Keng Hin <kenghin_lim@hotmail.com>
2020-08-04 17:59:18 +0800

    Gyoza r yum

---
+++ Assets/Scripts/Debug Menu/DrawGizmo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawGizmo : MonoBehaviour
+{
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(gameObject.transform.position, 1);
+    }
+}
