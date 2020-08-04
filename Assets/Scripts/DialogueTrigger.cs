ebcd05af8294a90aba10461ecbdea5a8ad920310
Bertha <bertha_han@mymail.sutd.edu.sg>
2020-08-04 18:01:57 +0800

    added animation for player bedroom and town scene

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    public Dialogue dialogue;
+    //public Dialogue dialogue;
+    //public GameObject dialogueBox;
+    public Animation anim;
+
+    void OnAwake(){
+        anim = GetComponent<Animation>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log(collider.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+        if(collider.gameObject.CompareTag("Gatekeeper")){
+            Debug.Log(collider.tag);
+            if (Input.GetKeyDown("E")){
+                print("E key was pressed");
+                anim.Play();
+            }
+        }
+    }
 }
