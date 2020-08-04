533dbaf39c781eadb3d4c51a9bb22e28166fbd66
Tan Wei Jin <weijin_tan@mymail.sutd.edu.sg>
2020-08-04 20:13:30 +0800

    Create barrier for rooms

---
+++ Assets/Scripts/BoundaryManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryManager : MonoBehaviour
+{
+    private Camera cam;
+    public Renderer spriteRenderer;
+    public GameObject[] boundaries;
+    public GameObject[] enemies;
+
+    private void Start() {
+        cam = Camera.main;
+    }
+    void Update()
+    {
+        if (cam.tag == "MainCamera")
+        {
+            if (spriteRenderer.isVisible == false)
+            {
+                foreach (GameObject boundary in boundaries)
+                {
+                    boundary.SetActive(false);
+                }
+            }
+            else
+            {
+                foreach (GameObject boundary in boundaries)
+                {
+                    boundary.SetActive(true);
+                }
+            }
+        }
+        bool anyEnemyAlive = false;
+        foreach (GameObject enemy in enemies)
+        {
+            if (enemy.activeSelf == true)
+            {
+                anyEnemyAlive = true;
+                break;
+            }
+        }
+        if (anyEnemyAlive == false)
+        {
+            foreach (GameObject boundary in boundaries)
+            {
+                boundary.SetActive(false);
+            }
+        }
+    }
+}
