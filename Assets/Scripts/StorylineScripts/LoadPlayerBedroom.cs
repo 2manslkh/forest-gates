06d99c1fc65e52e2681e75041ef0196c9e6f9aa1
Bertha <bertha_han@mymail.sutd.edu.sg>
2020-08-05 14:18:03 +0800

    linked from start menu to end of storyline + added transitions

---
+++ Assets/Scripts/StorylineScripts/LoadPlayerBedroom.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadPlayerBedroom : MonoBehaviour
+{
+    public void LoadBedroomScene()
+     {
+         SceneManager.LoadScene("1_Bedroom");        
+     }
+}
