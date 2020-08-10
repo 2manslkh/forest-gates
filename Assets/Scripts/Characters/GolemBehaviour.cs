24659154c160431500913316611059c4c6a0438a
Tan Wei Jin <weijin_tan@mymail.sutd.edu.sg>
2020-08-10 13:53:12 +0800

    KamekameRockWall

--- Assets/Scripts/Characters/GolemBehaviour.cs
+++ Assets/Scripts/Characters/GolemBehaviour.cs
@@ -6,10 +6,12 @@ public class GolemBehaviour : MonoBehaviour
 {
     private Transform playerPos;
     public GameObject rockSpawnPrefab;
+    public GameObject rockWallPrefab;
+
     private Camera cam;
     private enum State {
         Follow,
-        Attack,
+        Wall,
         Slam,
     }
     private State state;
@@ -21,19 +23,25 @@ public class GolemBehaviour : MonoBehaviour
     private bool Slamming;
     private Vector3 currentPatrolSpot;
     public float timeToSlam;
+    public float timeToAttack;
     private bool readySlam;
     private float rockSize;
     private bool doneSlamming;
+    private bool doneWalling;
+    public float awayDistance;
     // Start is called before the first frame update
     private void Awake() {
         state = State.Follow;
         animator = GetComponent<Animator>();
         characterStats = GetComponent<CharacterStats>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-        speed = 0.7f;
-        timeToSlam = 2;
+        speed = 2f;
+        timeToSlam = 5;
+        timeToSlam = 6;
         rockSize = 3f;
         cam = Camera.main;
+        awayDistance = 8f;
+        attackDistance = 3f;
     }
     void Start()
     {
@@ -44,40 +52,51 @@ public class GolemBehaviour : MonoBehaviour
     void Update()
     {
         Vector2 difference = playerPos.position - animator.transform.position;
+        print(difference.normalized);
+
         switch (state) {
             default:
             case State.Follow:
                 timeToSlam -= Time.deltaTime;
                 if (timeToSlam < 0)
                 {
+                    animator.SetBool("isFollowing", false);
                     state = State.Slam;
                     timeToSlam = 5;
                 }
 
-                animator.transform.position = Vector2.MoveTowards(animator.transform.position, playerPos.position, speed * Time.deltaTime);
+                if (difference.magnitude < awayDistance) {
+                    animator.transform.position = Vector2.MoveTowards(animator.transform.position, -difference.normalized* 10, speed * Time.deltaTime);
+		        }
                 animator.SetBool("isFollowing", true);
                 animator.SetFloat("Horizontal", difference.x);
                 animator.SetFloat("Vertical", difference.y);
-
                 
 
-                if (difference.magnitude <= attackDistance) {
-                    state = State.Attack;
-                    animator.SetBool("isFollowing", false);
+                if (difference.magnitude <= attackDistance || difference.magnitude >= awayDistance) {
+                    timeToAttack -= Time.deltaTime;
+                    if (timeToAttack < 0)
+                    {
+                        print("attacking");
+                        state = State.Wall;
+                        animator.SetBool("isFollowing", false);
+                        timeToAttack = 6;
+                    }
                 }
                 break;
-            case State.Attack:
-                animator.SetBool("isAttacking", true);
-
-                if(animator.GetCurrentAnimatorStateInfo(0).length < animator.GetCurrentAnimatorStateInfo(0).normalizedTime && animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+            case State.Wall:
+                if (doneWalling)
                 {
+                    animator.SetBool("isAttacking", false);
+                    if(!animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+                    {
+                        doneWalling = false;
+                        state = State.Follow;
+                    }
+                } else {
                     animator.SetFloat("Horizontal", difference.x);
                     animator.SetFloat("Vertical", difference.y);
-                }
-
-                if (difference.magnitude > attackDistance) {
-                    state = State.Follow;
-                    animator.SetBool("isAttacking", false);
+                    animator.SetBool("isAttacking", true);
                 }
                 break;
 
@@ -88,7 +107,7 @@ public class GolemBehaviour : MonoBehaviour
                     if(!animator.GetCurrentAnimatorStateInfo(0).IsName("Slam"))
                     {
                         doneSlamming = false;
-                        state = State.Attack;
+                        state = State.Follow;
                     }
                 } else {
                     animator.SetFloat("Horizontal", difference.x);
@@ -111,6 +130,37 @@ public class GolemBehaviour : MonoBehaviour
             var rock = Instantiate(rockSpawnPrefab, randomPosition, Quaternion.identity);
         }
         doneSlamming = true;
+    }
+
+
+    public void SpawnWall()
+    {
+        Vector2 difference = playerPos.position - animator.transform.position;
+        Vector2 normalized = difference.normalized;
+        Vector3 offset = Vector3.zero;
+        if(normalized.x > 0 && (Mathf.Abs(normalized.x) > Mathf.Abs(normalized.y)))
+        {
+            //right
+            offset = animator.transform.right*4;
+        } else if (normalized.x < 0 && (Mathf.Abs(normalized.x) > Mathf.Abs(normalized.y)))
+        {
+            //left
+            offset = -animator.transform.right*4;
+        } else if (normalized.y > 0 && (Mathf.Abs(normalized.y) > Mathf.Abs(normalized.x)))
+        {
+            //up
+            offset = animator.transform.up*4;
+        } else if (normalized.y < 0 && (Mathf.Abs(normalized.y) > Mathf.Abs(normalized.x)))
+        {
+            //down
+            offset = -animator.transform.up*4;
+        }
+        print(offset);
+        Vector3 spawnPosition = new Vector3(animator.transform.position.x + normalized.x, animator.transform.position.y + normalized.y, animator.transform.position.z);
+        var rockWall = Instantiate(rockWallPrefab, spawnPosition + offset, Quaternion.identity);
+        print(animator.transform.right * 10);
+        rockWall.GetComponent<RockWallBehaviour>().UpdateAnimator(difference.x, difference.y);
+        doneWalling = true;
     }
 
 }
